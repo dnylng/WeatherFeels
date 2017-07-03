710eaa3c6cf3e0cbcccff63528bede2a65f0b36a
dnylng <iamdannyluong@gmail.com>
2017-07-03 14:57:18 -0400

    Implemented the table view
    The table view now shows date, weather, and temps. I will pull data from online to update the cell next.

--- WeatherVC.swift
+++ WeatherVC.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var dateLbl: UILabel!
+    
+    @IBOutlet weak var currentTempLbl: UILabel!
+    
+    @IBOutlet weak var locationLbl: UILabel!
+    
+    @IBOutlet weak var currentWeatherImg: UIImageView!
+    
+    @IBOutlet weak var currentWeatherTypeLbl: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    
+    // Number of columns
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // Number of rows, 6 other days in a week
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    // Create a reusable cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+    
 }
 
