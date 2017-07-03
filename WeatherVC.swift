bc37c44fb527a46c303a16d97a1c8f0b41cff860
dnylng <iamdannyluong@gmail.com>
2017-07-03 13:18:45 -0400

    Started on the UI layout of labels and images
    Not finalized UI. I've put items into their respective stack views.

--- WeatherVC.swift
+++ WeatherVC.swift
@@ -12,14 +12,7 @@ class WeatherVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
